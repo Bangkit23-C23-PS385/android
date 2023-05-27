61f802cf0a2d9ce1d52fe0783b4506740e6dddcc
gybran <charistomarcgybran@students.undip.ac.id>
2023-05-27 21:47:07 +0700

    profile page init

--- app/src/main/java/com/bangkitacademy/medicare/ui/profil/ProfilViewModel.kt
+++ app/src/main/java/com/bangkitacademy/medicare/ui/profil/ProfilViewModel.kt
@@ -1,10 +1,10 @@
-package com.bangkitacademy.medicare.ui.notifications
+package com.bangkitacademy.medicare.ui.profil
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NotificationsViewModel : ViewModel() {
+class ProfilViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is notifications Fragment"

aaf7d860bcfb2e67bd6b621b8224ac6382871026
Faradayy05 <muhammadfarisadityapratama05@gmail.com>
2023-06-03 22:22:45 +0700

    add prediction feature

--- app/src/main/java/com/bangkitacademy/medicare/ui/prediction/PredictionViewModel.kt
+++ app/src/main/java/com/bangkitacademy/medicare/ui/prediction/PredictionViewModel.kt
@@ -1,10 +1,10 @@
-package com.bangkitacademy.medicare.ui.dashboard
+package com.bangkitacademy.medicare.ui.prediction
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DashboardViewModel : ViewModel() {
+class PredictionViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is dashboard Fragment"

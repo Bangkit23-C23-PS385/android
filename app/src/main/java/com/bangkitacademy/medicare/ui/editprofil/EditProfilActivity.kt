566efb3954dc8148a71cb9307196ff3537ed389a
gybran <charistomarcgybran@students.undip.ac.id>
2023-06-06 01:07:10 +0700

    format tgl, jenis kelamin, pilih gambar

--- app/src/main/java/com/bangkitacademy/medicare/ui/editprofil/EditProfilActivity.kt
+++ app/src/main/java/com/bangkitacademy/medicare/ui/editprofil/EditProfilActivity.kt
@@ -5,15 +5,23 @@ import android.content.Intent.ACTION_GET_CONTENT
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.InputType
+import android.util.Log
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.widget.AppCompatRadioButton
 import com.bangkitacademy.medicare.databinding.ActivityEditProfilBinding
+import com.bangkitacademy.medicare.utils.dateFormatToDb
 import com.bangkitacademy.medicare.utils.dateFormatToUser
 import com.bangkitacademy.medicare.utils.uriToFile
 import com.google.android.material.datepicker.MaterialDatePicker
+import java.io.File
 
 class EditProfilActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityEditProfilBinding
+    private var birthDate: String? = null
+    private var getFile: File? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,6 +29,7 @@ class EditProfilActivity : AppCompatActivity() {
 
         binding = ActivityEditProfilBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
 
         // handle top app bar back button
         binding.toolBar.setNavigationOnClickListener {
@@ -32,28 +41,35 @@ class EditProfilActivity : AppCompatActivity() {
             startGallery()
         }
 
+        binding.btnSimpan.setOnClickListener {
+            uploadData()
+        }
+
         // handle date picker
-        openDatePicker()
+        binding.edtTanggalLahir.setOnClickListener {
+            openDatePicker()
+        }
 
     }
 
+    /*
+     * open date picker when date of birth edit text clicked
+     */
     private fun openDatePicker() {
-        val datePicker = MaterialDatePicker.Builder.datePicker()
-            .setTitleText("Pilih Tanggal Lahir")
-            .setSelection(MaterialDatePicker.todayInUtcMilliseconds())
-            .build()
+        val datePicker = MaterialDatePicker.Builder.datePicker().setTitleText("Pilih Tanggal Lahir")
+            .setSelection(MaterialDatePicker.todayInUtcMilliseconds()).build()
 
-        binding.edtTanggalLahir.setOnClickListener {
-            datePicker.show(supportFragmentManager, "DATE_PICKER")
+        datePicker.show(supportFragmentManager, "DATE_PICKER")
 
-            datePicker.addOnPositiveButtonClickListener { selectedDate ->
-                binding.edtTanggalLahir.setText(datePicker.headerText.dateFormatToUser())
-            }
+        datePicker.addOnPositiveButtonClickListener { selectedDate ->
+            birthDate = datePicker.headerText.dateFormatToDb()
+            binding.edtTanggalLahir.setText(datePicker.headerText.dateFormatToUser())
+            binding.edtTanggalLahir.error = null
         }
     }
 
     /*
-    * start gallery intent
+     * start gallery intent
      */
     private fun startGallery() {
         val intent = Intent()
@@ -64,7 +80,7 @@ class EditProfilActivity : AppCompatActivity() {
     }
 
     /*
-    * handle result from gallery intent
+     * handle result from gallery intent
      */
     private val launcherIntentGallery = registerForActivityResult(
         ActivityResultContracts.StartActivityForResult()
@@ -73,8 +89,58 @@ class EditProfilActivity : AppCompatActivity() {
             val selectedImg = result.data?.data as Uri
             selectedImg.let { uri ->
                 val myFile = uriToFile(uri, this@EditProfilActivity)
+                getFile = myFile
                 binding.imgFotoProfil.setImageURI(uri)
             }
+        }
+    }
+
+    /*
+     * upload new data user to database
+     */
+    private fun uploadData() {
+        val nama = binding.edtNama.text.toString()
+        val selectedJkId = binding.radioGroupJk.checkedRadioButtonId
+        val tanggalLahir = birthDate
+        val tinggiBadan = binding.edtTinggiBadan.text.toString()
+        val beratBadan = binding.edtBeratBadan.text.toString()
+        var jenisKelamin = ""
+
+        // get selected radio button
+        if (selectedJkId != -1) {
+            val selectedJk = binding.radioGroupJk.findViewById(selectedJkId) as AppCompatRadioButton
+            jenisKelamin = if (selectedJk.text == "Laki-laki") {
+                "L"
+            } else {
+                "P"
+            }
+        }
+
+        if (nama.isEmpty() || jenisKelamin.isEmpty() || tanggalLahir.isNullOrEmpty() || tinggiBadan.isEmpty() || beratBadan.isEmpty()) {
+            if (nama.isEmpty()) {
+                binding.edtNama.requestFocus()
+                binding.edtNama.error = "Nama tidak boleh kosong"
+            }
+            if (tanggalLahir.isNullOrEmpty()) {
+                binding.edtTanggalLahir.requestFocus()
+                binding.edtTanggalLahir.error = "Tanggal lahir tidak boleh kosong"
+            }
+            if (tinggiBadan.isEmpty()) {
+                binding.edtTinggiBadan.requestFocus()
+                binding.edtTinggiBadan.error = "Tinggi badan tidak boleh kosong"
+            }
+            if (beratBadan.isEmpty()) {
+                binding.edtBeratBadan.requestFocus()
+                binding.edtBeratBadan.error = "Berat badan tidak boleh kosong"
+            }
+            if (jenisKelamin.isEmpty()) {
+                Toast.makeText(this, "Jenis kelamin tidak boleh kosong", Toast.LENGTH_SHORT).show()
+            }
+        } else {
+            Log.d(
+                "Test",
+                "uploadData: $nama, $jenisKelamin, $tanggalLahir, $tinggiBadan, $beratBadan $getFile"
+            )
         }
     }
 

249742f36e52b3756dbf13171967332cf0409d44
gybran <charistomarcgybran@students.undip.ac.id>
2023-06-05 18:34:17 +0700

    format tgl, jenis kelamin, pilih gambar

--- app/src/main/java/com/bangkitacademy/medicare/ui/editprofil/EditProfilActivity.kt
+++ app/src/main/java/com/bangkitacademy/medicare/ui/editprofil/EditProfilActivity.kt
@@ -1,12 +1,14 @@
 package com.bangkitacademy.medicare.ui.editprofil
 
+import android.content.Intent
+import android.content.Intent.ACTION_GET_CONTENT
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.Toast
-import com.bangkitacademy.medicare.R
+import androidx.activity.result.contract.ActivityResultContracts
 import com.bangkitacademy.medicare.databinding.ActivityEditProfilBinding
-import com.bangkitacademy.medicare.databinding.ActivityMainBinding
+import com.bangkitacademy.medicare.utils.dateFormatToUser
+import com.bangkitacademy.medicare.utils.uriToFile
 import com.google.android.material.datepicker.MaterialDatePicker
 
 class EditProfilActivity : AppCompatActivity() {
@@ -25,7 +27,12 @@ class EditProfilActivity : AppCompatActivity() {
             onBackPressed()
         }
 
+        // handle button edit foto
+        binding.lytFotoProfil.setOnClickListener {
+            startGallery()
+        }
 
+        // handle date picker
         openDatePicker()
 
     }
@@ -40,7 +47,33 @@ class EditProfilActivity : AppCompatActivity() {
             datePicker.show(supportFragmentManager, "DATE_PICKER")
 
             datePicker.addOnPositiveButtonClickListener { selectedDate ->
-                binding.edtTanggalLahir.setText(datePicker.headerText)
+                binding.edtTanggalLahir.setText(datePicker.headerText.dateFormatToUser())
+            }
+        }
+    }
+
+    /*
+    * start gallery intent
+     */
+    private fun startGallery() {
+        val intent = Intent()
+        intent.action = ACTION_GET_CONTENT
+        intent.type = "image/*"
+        val chooser = Intent.createChooser(intent, "Pilih Foto Profil")
+        launcherIntentGallery.launch(chooser)
+    }
+
+    /*
+    * handle result from gallery intent
+     */
+    private val launcherIntentGallery = registerForActivityResult(
+        ActivityResultContracts.StartActivityForResult()
+    ) { result ->
+        if (result.resultCode == RESULT_OK) {
+            val selectedImg = result.data?.data as Uri
+            selectedImg.let { uri ->
+                val myFile = uriToFile(uri, this@EditProfilActivity)
+                binding.imgFotoProfil.setImageURI(uri)
             }
         }
     }
